7d3628af1b180784641e553af2c6a0b89d51a442
BoBeenLee <bobeen.lee@huiseoul.com>
2018-11-27 11:08:07 +0900

    코드 수정

--- storybook/stories/Button.stories.tsx
+++ storybook/stories/Button.stories.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
-import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react-native";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 import { Button } from "../../src/components";
 
-storiesOf('Button', module).add('Normal Button', () => <Button />);
+storiesOf("Button", module).add("Normal Button", () => (
+  <Button type="default">Hello</Button>
+));
