0db479f2edba63869135802931c543fba3849640
BoBeenLee <bobeen.lee@huiseoul.com>
2018-12-04 09:56:10 +0900

    코드 추가

--- storybook/stories/Card.stories.tsx
+++ storybook/stories/Card.stories.tsx
@@ -12,20 +12,29 @@ const Container = styled.View`
 `;
 
 const film = {
-  "id": "ZmlsbXM6MQ==",
-  "title": "A New Hope",
-  "episodeID": 4,
-  "created": "2014-12-10T14:23:31.880Z",
-  "openingCrawl": "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
-  "director": "George Lucas",
-  "producers": [
-    "Gary Kurtz",
-    "Rick McCallum"
-  ]
+  id: "ZmlsbXM6MQ==",
+  title: "A New Hope",
+  episodeID: 4,
+  created: "2014-12-10T14:23:31.880Z",
+  openingCrawl:
+    "It is a period of civil war.\r\nRebel spaceships, striking\r\nfrom a hidden base, have won\r\ntheir first victory against\r\nthe evil Galactic Empire.\r\n\r\nDuring the battle, Rebel\r\nspies managed to steal secret\r\nplans to the Empire's\r\nultimate weapon, the DEATH\r\nSTAR, an armored space\r\nstation with enough power\r\nto destroy an entire planet.\r\n\r\nPursued by the Empire's\r\nsinister agents, Princess\r\nLeia races home aboard her\r\nstarship, custodian of the\r\nstolen plans that can save her\r\npeople and restore\r\nfreedom to the galaxy....",
+  director: "George Lucas",
+  producers: ["Gary Kurtz", "Rick McCallum"]
 };
-
 
 storiesOf("Card", module)
   .addDecorator((getStory: any) => <Container>{getStory()}</Container>)
-  .add("FilmCard", () => <FilmCard title={film.title} created={moment(film.created).format("YYYY-MM-DD")} openingCrawl={film.openingCrawl} />)
-  .add("TodoCard", () => <TodoCard />);
+  .add("FilmCard", () => (
+    <FilmCard
+      title={film.title}
+      created={moment(film.created).format("YYYY-MM-DD")}
+      openingCrawl={film.openingCrawl}
+    />
+  ))
+  .add("TodoCard", () => (
+    <React.Fragment>
+      <TodoCard order={1} content={"First"} />
+      <TodoCard order={2} content={"Second"} />
+      <TodoCard order={3} content={"Third"} />
+    </React.Fragment>
+  ));
